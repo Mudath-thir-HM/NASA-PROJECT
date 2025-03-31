1781ded2cd43744b9a42ec4a22e81add866dc815
Mudath-thir-HM <eighthmudathir@gmail.com>
2025-03-31 19:15:50 +0100

    update Footer component to include links to the Front-End and Back-End codebases

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,27 @@ import { Footer as ArwesFooter, Paragraph } from "arwes";
 import Centered from "./Centered";
 
 const Footer = () => {
-  return <ArwesFooter animate>
-    <Centered>
-      <Paragraph style={{ fontSize: 14, margin: "10px 0" }}>
-        This is not an official site and is not affiliated with NASA or SpaceX in any way. For educational purposes only.
-      </Paragraph>
-    </Centered>
-  </ArwesFooter>
+  return (
+    <ArwesFooter animate>
+      <Centered>
+        <Paragraph style={{ fontSize: 14, margin: "10px 0" }}>
+          Visit the codebase at{" "}
+          <a
+            href="https://github.com/Mudath-thir-HM/NASA-PROJECT"
+            target="_self"
+          >
+            Front-End
+          </a>{" "}
+          <a
+            href="https://github.com/Mudath-thir-HM/NASA-BACKEND"
+            target="_self"
+          >
+            Back-End
+          </a>
+        </Paragraph>
+      </Centered>
+    </ArwesFooter>
+  );
 };
 
 export default Footer;
